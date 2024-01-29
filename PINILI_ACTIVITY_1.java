aef642625a0e010b83d4fc6daecfdcfb3039ccab
piniliace <158040288+piniliace@users.noreply.github.com>
2024-01-29 18:36:02 +0800

    Add files via upload

---
+++ PINILI_ACTIVITY_1.java
@@ -0,0 +1,18 @@
+public class PINILI_ACTIVITY_1{
+    public static void main(String []args) {
+        int x = 5;
+        int y = 10;
+        int z = 15;
+        
+        int sum = x + y + z;
+        int diff = x - y - z;
+        int product = x * y * z;
+        int quotient = x / y / z;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + diff);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+
+    }
+}
