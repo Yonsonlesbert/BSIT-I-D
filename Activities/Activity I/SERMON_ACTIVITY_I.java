66aeb93f26e943e20ea581df293ced2c538eed1e
kishaaltheaa <150760175+kishaaltheaa@users.noreply.github.com>
2024-01-29 18:12:29 +0800

    Add files via upload

---
+++ Activities/Activity I/SERMON_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class SERMON_ACTIVITY_I{
+    public static void main(String []args){
+        int a = 20, b = 10, c = 15;
+
+        int sum= a+b+c;
+        int diff= a-b;
+        int product= b*c;
+        int quotient= a/b;
+
+        System.out.println("Sum = "+sum);
+        System.out.println("Difference = "+diff);
+        System.out.println("Product = "+product);
+        System.out.println("Quotient = "+quotient);
+    }
+}
+
+    
+
