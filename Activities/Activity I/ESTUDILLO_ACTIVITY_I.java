3185a2b925393b7651fbd06dcbffdd3c794f3b67
Estudillostephanie <157589357+Estudillostephanie@users.noreply.github.com>
2024-01-29 18:22:26 +0800

    Update ESTUDILLO_ACTIVITY_I.java

--- Activities/Activity I/ESTUDILLO_ACTIVITY_I.java
+++ Activities/Activity I/ESTUDILLO_ACTIVITY_I.java
@@ -1,8 +1,8 @@
-public class ESTUDILLO_ACTIVITY I {
+public class ESTUDILLO_ACTIVITY_I{
     public static void main(String[]args){
         int x = 20, y = 30, z = 50;
         
-        int sum = x + y + z;
+        int sum = 
         int diff = x - y - z;
         int prod = x * y * z;
         int qout = x / y / z;
