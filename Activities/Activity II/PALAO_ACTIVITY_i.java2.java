334948485850a87f884c46d54b01cbb515827224
joshpalao20 <158041420+joshpalao20@users.noreply.github.com>
2024-01-29 19:02:16 +0800

    Add files via upload

---
+++ Activities/Activity II/PALAO_ACTIVITY_i.java2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PALAO_ACTIVITY_II {
+public static void main(String[]args){
+    Scanner sc = new Scanner (System.in);
+    System.out.print("s: Enter First Number");
+    int num1 = sc.nextInt();
+
+    System.out.print("s: Enter Second Number");
+    int num2 = sc.nextInt();
+
+    int = sum1 + sum2;
+    int = sum1 - sum2;
+    int = sum1 * sum2;
+
+    System.out.println("Sum"+ sum);
+    System.out.println("Difference"+ Difference) ;
+    System.out.println("Product"+ Product);
+ }  
+}
