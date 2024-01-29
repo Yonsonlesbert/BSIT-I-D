c0803951b2adb728c24c66426e499719868e8105
joshpalao20 <158041420+joshpalao20@users.noreply.github.com>
2024-01-29 18:36:49 +0800

    Add files via upload

---
+++ Activities/Activity I/PALAO_ACTIVITY_i.java
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace.std
+
+int main(){
+    int i = 10
+    int a = 20
+    int b = 30
+ 
+
+     int sum = i + a + b
+     int diff = b - i
+     int quotient = b / i
+     int product = b * i
+
+     count << "sum: << i + a + b;
+     count << "diff : <<  b - i;
+     count << "quotient: << b / i;
+     count << "product: << b * i;
+}
