8374250f59208844c569a87343e09ed849f1be4c
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-01-29 18:21:44 +0800

    Add files via upload

---
+++ Activities/Activity I/ALLEGO_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+public class ALLEGO_ACTIVITY_I{
+    public static void main(String[] args){
+        int a = 10, b = 20, c = 30;
+     
+      int sum = a + b + c;
+      int diff = a - b - c;
+      int product = a*b*c;
+      int quotient = a/b/c;
+  
+      System.out.println("Sum =" + sum);
+      System.out.println("Difference =" + diff);
+      System.out.println("Product=" + product);
+      System.out.println("Quotient =" + quotient);
+      
+    }
+ }
