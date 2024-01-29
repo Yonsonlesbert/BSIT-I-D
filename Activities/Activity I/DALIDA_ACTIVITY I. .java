348fdf2cbe231fa154b805677f94bab1bab6c8a0
Lydivine Dalida <157588963+lydivinegakodlida04@users.noreply.github.com>
2024-01-29 18:21:16 +0800

    Add files via upload

---
+++ Activities/Activity I/DALIDA_ACTIVITY I. .java
@@ -0,0 +1,10 @@
+public class DALIDA_ACTIVITY I
+public static void main(String[]args){
+   int a = 20, b = 30, c = 40;
+
+    System.out.println("sum is "=  a);
+    System.out.println("diff is "+ b);
+    System.out.println("product");
+
+    
+}
