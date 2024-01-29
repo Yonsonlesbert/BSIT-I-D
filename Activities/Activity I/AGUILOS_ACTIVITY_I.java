c95c3d3a84accba5aa5a8ce1ba8cd8f6dacf1a39
GiaGraceVansie <158039956+GiaGraceVansie@users.noreply.github.com>
2024-01-29 18:19:40 +0800

    Add files via upload

---
+++ Activities/Activity I/AGUILOS_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class AGUILOS_ACTIVITY_I{
+    public static void main (String[]args){
+       int x = 4;
+       int y = 5;
+       int z = 7;
+
+       int sum = x + y + z;
+       int diff = x - y - z;
+       int product = x * y * z;
+       int quotient = x / y / z;
+
+       System.out.println("Sum = " + sum);
+       System.out.println( "Difference = " + diff);
+       System.out.println("Product = " + product);
+       System.out.println("Quotient = " + quotient);
+    }
+}
