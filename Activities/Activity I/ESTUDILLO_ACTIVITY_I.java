d634157e3c7d1a36de903ac72c9dbd830d694970
Estudillostephanie <157589357+Estudillostephanie@users.noreply.github.com>
2024-01-29 18:12:50 +0800

    Add files via upload

---
+++ Activities/Activity I/ESTUDILLO_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+public class ESTUDILLO_ACTIVITY I {
+    public static void main(String[]args){
+        int x = 20, y = 30, z = 50;
+        
+        int sum = x + y + z;
+        int diff = x - y - z;
+        int prod = x * y * z;
+        int qout = x / y / z;
+
+        System.out.print
+
+    
+    }
+}
