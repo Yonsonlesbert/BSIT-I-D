88c94cdabd5ea1de5af8b3b9aa8e8d4a93fec5b5
laarnie20 <158042185+laarnie20@users.noreply.github.com>
2024-01-29 18:42:47 +0800

    Add files via upload

---
+++ Activities/Activity I/CAPACITE_ACTIVITY_1.java
@@ -0,0 +1,18 @@
+public class CAPACITE_ACTIVITY_1 {
+    public static void main(String[] args) {
+        int x = 10, y = 20, z = 30;
+
+        int sum = x + y + z;
+        int diff = z - y -x;
+        int prod = x * y * z;
+        int qout = z / y/ x;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Diff: "+diff);
+        System.out.println("Prod: "+prod);
+        System.out.println("Qout:"+qout);
+    }
+}
+
+
+
