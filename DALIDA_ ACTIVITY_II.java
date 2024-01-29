a6a20570b1415330fbacb769edea8f8e4497271d
Lydivine Dalida <157588963+lydivinegakodlida04@users.noreply.github.com>
2024-01-29 19:02:24 +0800

    Add files via upload

---
+++ DALIDA_ ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class DALIDA_ACTIVITY_II{
+    public static void main(String [] args){
+
+    
+
+        Scanner sc + new Scanner(System.in);
+        
+        System.out.print("First number: ");
+        int fnum = sc.nextlnt;
+        System.out.print("Second number: ");
+        int snum
+
+    }
+}
