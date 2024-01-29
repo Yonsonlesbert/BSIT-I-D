f4c1d18aa0d15e958e0db898072e5db50323295f
Leonises <157588921+calunsagleonises@users.noreply.github.com>
2024-01-29 18:30:48 +0800

    Add files via upload

---
+++ Activities/Activity I/CALUNSAG_ACTIVIY_I.java
@@ -0,0 +1,19 @@
+public class CALUNSAG_ACTIVITY_I{
+    public static void main(String[]args){
+      int x = 9, y = 28, z = 31;
+    
+      int sum = x+y+z;
+      int diff = x-y-z;
+      int quot = x/y/z;
+      int prod = x*y*z;
+    
+
+      System.out.println("Sum: " + sum);
+      System.out.println("Difference: " + diff);
+      System.out.println("Quotient: " + quot);
+      System.out.println("Product: " + prod); 
+    
+      }
+    }
+    
+    
