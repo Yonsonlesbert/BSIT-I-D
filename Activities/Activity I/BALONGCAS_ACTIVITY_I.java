5ae8841ff5a9069d6e62f119aaa9d83c03743ac3
Balongcas <157291749+AljhoBalongcas@users.noreply.github.com>
2024-01-29 18:15:47 +0800

    Add files via upload

---
+++ Activities/Activity I/BALONGCAS_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class BALONGCAS_ACTIVITY_I{
+    public static void main(String []args){
+
+        int x = 5;
+        int y = 15;
+        int z = 20;
+
+        int addition = x+y+z;
+        int subtarction = addition-z;
+        int multiplication = addition*subtarction;
+        int division = multiplication/addition;
+
+        System.out.println("Sum= " + addition);
+        System.out.println("Diff= " + subtarction);
+        System.out.println("Product= " + multiplication);
+        System.out.println("Quotient= " + division);
+    }
+}
