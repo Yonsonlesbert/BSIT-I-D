4ee01bb1670100f74e63afd558a4d6a8a7017645
Joelpalacio <157589826+Joelpalacio@users.noreply.github.com>
2024-01-29 18:27:30 +0800

    Add files via upload

---
+++ Activities/Activity I/PALACIO_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+ public class PALACIO_ACTIVITY_I {
+    public static void main(String[] args) {
+        int a= 15, b=20, c=25;
+
+        int addition = a+b+c;
+        int subtarction = a-c;
+        int multiplication = b*subtarction;
+        int division = multiplication/addition;
+
+        System.out.println("Sum: " + addition);
+        System.out.println("Diff: " + subtarction);
+        System.out.println("Product: " + multiplication);
+        System.out.println("Quotient: " + division);
+
+    }
+ 
+    
+ }
