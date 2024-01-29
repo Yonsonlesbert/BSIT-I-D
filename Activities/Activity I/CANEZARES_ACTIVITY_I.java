fbf7b9f26560f7deb1fbec5e8308b1bdda478f63
Carlo P. Cañezares <156906555+CarloPC@users.noreply.github.com>
2024-01-29 18:11:57 +0800

    Add files via upload

---
+++ Activities/Activity I/CANEZARES_ACTIVITY_I.java
@@ -0,0 +1,19 @@
+public class CANEZARES_ACTIVITY_I {
+    public static void main (String[] args) {
+
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        int Product = x * y * z;
+        int Sum = x + y + z;
+        int Difference = x - y - z;
+        int Qoutient = x / y / z;
+
+        System.out.println("Product:"+ Product);
+        System.out.println("Sum: "+ Sum);
+        System.out.println("Difference: "+ Difference);
+        System.out.println("Qoutient: "+ Qoutient);
+        
+    }
+}
