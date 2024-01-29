5268eb15cb3515e4336a44490d08903bad201219
warr90 <158041134+warr90@users.noreply.github.com>
2024-01-29 18:46:20 +0800

    Add files via upload

---
+++ Activities/Activity I/public class DELFIN_ACTIVITY_I.java
@@ -0,0 +1,19 @@
+public class DELFIN_ACTIVITY_I.java;
+
+public static Variables {
+public static void main (String []args) {
+       int a = 25;
+       int b = 50;
+       int c = 75;
+       
+       int sum = a + b;
+       int diff = c - b;
+       int prod = a * b * c;
+       int qout = a / b / c;
+       
+       System.out.println("Sum: + sum");
+       System.out.println("Diff: - diff");
+       System.out.println("Prod: * prod");
+       System.out.println("Quot: / qout");
+   } 
+}
