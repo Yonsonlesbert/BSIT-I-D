61a75eedec45a903bd80baf88ef0c5f3238923ab
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-01-29 18:14:05 +0800

    Add files via upload

---
+++ Activities/Activity I/LAÑOJAN_ACTIVITY_I.java
@@ -0,0 +1,11 @@
+public class LAÑOJAN_ACTIVITY_I{
+    public static void main (String[] args){
+int a = 100, b = 50,  c = 40;
+
+System.out.println("The sum is:" + (a+b+c));
+System.out.println("The diff is:" + (a-b-c));
+System.out.println("The product is:" + (a*b*c));
+System.out.println("The quotient is:" + (a/b));
+
+    }
+}
