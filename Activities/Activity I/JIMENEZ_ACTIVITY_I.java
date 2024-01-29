e64202c52b483d83f9a112b297c2ba39b1561e22
James Ivan Jimenez <156911118+jamesezrampage12@users.noreply.github.com>
2024-01-29 18:12:25 +0800

    Add files via upload

---
+++ Activities/Activity I/JIMENEZ_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+public class JIMENEZ_ACTIVITY_I{
+    public static void main (String [] args){
+        int a = 10;
+        int b = 2;
+        int c = 5;
+        int z = a+b;
+        int x = a-c;
+        int v = b*c;
+        int n = a/c;
+
+        System.out.println("sum is "+ z);
+        System.out.println("diff is "+ x);
+        System.out.println("product is "+ v);
+        System.out.println("qou is "+ n);
+    }
+}
