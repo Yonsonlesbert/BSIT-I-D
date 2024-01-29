abaa57d112a563b6fb660f9b4f0f555bccb94c17
Leonises <157588921+calunsagleonises@users.noreply.github.com>
2024-01-29 19:02:04 +0800

    Add files via upload

---
+++ Activities/Activity II/CALUNSAG_ACTIVITY_II.java
@@ -0,0 +1,26 @@
+import java.util.Scanner{
+    public class CALUNSAG_ACTIVITY_II{
+        public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in) ;
+      
+        System.out.print(s:Enter your first number);
+        int firstNumber=scanner.nextInt();
+        System.out.print(s:Enter your second number);
+        int secondNumber=scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int prod = firstNumber * secondNumber;
+
+        System.out.println("Sum="+ sum);
+        System.out.println("Difference="+ difference);
+        System.out.println("Product="+ product);
+
+
+    }
+}     
+
+
+        
+    
