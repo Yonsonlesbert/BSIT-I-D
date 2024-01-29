321ba37328763ca098f7ec1e23d58b20a567ac71
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-01-29 18:12:23 +0800

    Add files via upload

---
+++ Activities/Activity I/MAURIN_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+public class MAURIN_ACTIVITY_I{
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 15;
+        int c = 20;
+
+        int sum = a+b;
+        int difference = c-b;
+        int product = b*b;
+        int quotient = c/a;
+
+        System.out.println ("Sum is "+ sum);
+        System.out.println ("Difference is "+ difference);
+        System.out.println ("Product is "+ product);
+        ystem.out.println ("Quotient is "+ quotient);
+
+
+
+
+    }
+}
+
