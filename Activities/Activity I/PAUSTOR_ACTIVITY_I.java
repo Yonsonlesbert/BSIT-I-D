4bd0e30c76203e2329285e147225e627adcccabe
carl160 <156910039+carl160@users.noreply.github.com>
2024-01-29 18:25:34 +0800

    Add files via upload

---
+++ Activities/Activity I/PAUSTOR_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class PAUSTOR_ACTIVITY_I {
+    public static void main(String[] args) {
+        
+        int x,y,z,sum,diff,prod,div;
+
+        x=10;
+        y=20;
+        z=30;
+
+        sum=x+y+z;
+        diff=x+y+z;
+        prod=x*y*z;
+        div=x/y/z;
+
+        System.out.println("sum= " +sum);
+        System.out.println("diff= " +diff);
+        System.out.println("product= " +prod);
+        System.out.println("quotient= " +div);
+    }
+}
