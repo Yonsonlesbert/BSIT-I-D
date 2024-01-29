65bbd3674e6d9d3501805636465be6674cef77bb
QueenLaurenADivivar <158040685+QueenLaurenADivivar@users.noreply.github.com>
2024-01-29 18:22:56 +0800

    Add files via upload

---
+++ Activities/Activity I/DIVIVAR_ACTIVITY1.java
@@ -0,0 +1,18 @@
+public class DIVIVAR_ACTIVITY1{
+    public static void main(String[]args){
+       int a = 10;
+       int b = 5;
+       int c = 2;
+   
+       int sum = a+b+c;
+       int diff = a-b-c;
+       int product = a*b*c;
+       int quotient = a/b/c;
+
+       System.out.println("Sum is:" +sum);
+       System.out.println("Diff is:" +diff);
+       System.out.println("Product is:" +product);
+       System.out.println("Quotient is:" +quotient);
+    }
+}
+
