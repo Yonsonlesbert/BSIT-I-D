e8ab91e942289f8982f0c369b2edbdf2d61c8da9
llordfrance <157589942+llordfrance@users.noreply.github.com>
2024-01-29 20:45:00 +0800

    Update LARVIO_ACTIVITY_II.java

--- Activities/Activity II/LARVIO_ACTIVITY_II.java
+++ Activities/Activity II/LARVIO_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ALLEGO_ACIVITY_II {
+public class LARVIO_ACIVITY_II {
     
 public static void main (String [] args){
   Scanner sc = new Scanner (System.in);
