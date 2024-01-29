f354611e512cb61d413e568076503425845f6fc2
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-01-29 19:00:19 +0800

    Add files via upload

---
+++ Activities/Activity II/LAÑOJAN_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LAÑOJAN_ACTIVITY_II{
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter First Number:");
+    int firstnum1= sc.nextInt();
+    System.out.print("Enter Second Number:");
+    int secondnum2= sc.nextInt();
+
+    System.out.println("The sum is:" +(firstnum1+secondnum2) );
+    System.out.println("The diff is:" +(firstnum1-secondnum2) );
+    System.out.println("The product is:" +(firstnum1*secondnum2) );
+   
+
+
+
+
+
+    }
+
+}
