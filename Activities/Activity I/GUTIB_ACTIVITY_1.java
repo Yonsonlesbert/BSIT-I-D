4348b2bdbee1bf25629f457be69e8374d801b254
JOHNLOYD-svg <156911022+JOHNLOYD-svg@users.noreply.github.com>
2024-01-29 18:28:44 +0800

    Add files via upload

---
+++ Activities/Activity I/GUTIB_ACTIVITY_1.java
@@ -0,0 +1,20 @@
+public class GUTIB_ACTYIVITY_1{
+          public static void main(String[]args){
+ 
+
+     int a = 14;
+     int b = 16;
+     int c = 18;
+
+     int sum  = a+b+c;
+     int diff = a-b-c;
+     int prod  = a*b*c;
+     int qout = a/b/c;
+
+
+     System.out.println("The sum is:" + sum);
+     System.out.println("The diff is:" + diff);
+     System.out.println("The prod is:" + prod);
+     System.out.println("The qout is:" + qout);
+    }
+}
