bd71a107a064179ba1f9c278b1a6ced5a4d67ae5
artdakay1 <158040099+artdakay1@users.noreply.github.com>
2024-01-29 18:26:26 +0800

    Add files via upload

---
+++ Activities/Activity I/Dakay_Activity_I.java
@@ -0,0 +1,13 @@
+public class Dakay_Activity_I {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3;
+        int sum = a+b+c;
+        int dif = a-b-c;
+        int pro = a*b*c;
+        int quo = a/b/c;
+        System.out.println("Sum "+sum);
+        System.out.println("Difference "+dif);
+        System.out.println("Product "+pro);
+        System.out.println("Quotient "+quo);
+    }
+}
