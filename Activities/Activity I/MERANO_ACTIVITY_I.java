e8421a74e0c70df63ca39878679e00d80a27cd8a
Kathleen Merano <157017491+mkathleenn@users.noreply.github.com>
2024-01-29 18:11:58 +0800

    Add files via upload

---
+++ Activities/Activity I/MERANO_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class MERANO_ACTIVITY_I{
+    public static void main (String [] args){
+       int a=35, b=25, c=5;
+
+       int sum = a+b+c;
+       int diff = a-b-c;
+       int product = c*b*a;
+       int quotient = a/c;
+
+       System.out.println("Sum = "+ sum);
+       System.out.println("Difference = "+ diff);
+       System.out.println("Product = "+ product);
+       System.out.println("quotient = "+ quotient);
+
+    }
+
+}
