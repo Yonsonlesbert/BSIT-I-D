13857793aefd57892cf5a3fe392bde4830e96182
piniliace <158040288+piniliace@users.noreply.github.com>
2024-01-29 19:04:44 +0800

    Add files via upload

---
+++ Activities/Activity II/PINILI_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PINILI_ACTIVITY_II{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("First number: ");
+        int fnum = sc.nextlnt();
+        System.out.print("Second number: ");
+        int snum = sc.nextlnt();
+
+        int sum = fnum + sum;
+        int diff = fnum - sum;
+        int product = fnum * sum;
+        int quotient = fnum / sum;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + diff);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+    }
+}
