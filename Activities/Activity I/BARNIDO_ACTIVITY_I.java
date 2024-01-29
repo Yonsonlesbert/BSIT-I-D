e2f26105576a22ec6452fc925dbca31a0b3ba818
jevs7714 <157568642+jevs7714@users.noreply.github.com>
2024-01-29 18:11:54 +0800

    Add files via upload

---
+++ Activities/Activity I/BARNIDO_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+public class BARNIDO_ACTIVITY_I {
+    public static void main(String[] args) {
+
+    //Assigning data type to the variables
+     int x, y, z, add,sub, mult, div;
+
+    //Assigning value to the variables
+     x=7;
+     y=14;
+     z=1;
+    
+    //Using simple equations
+    add=x+y+z;
+    sub=x-y-z;
+    mult=x*y*z;
+    div=x/y/z;
+
+    //Display the ouput or the result
+     System.out.println("Sum= "+add);
+     System.out.println("Difference= " +sub);
+     System.out.println("Product= "+mult);
+     System.out.println("Quotient= "+div);
+    }
+  }
