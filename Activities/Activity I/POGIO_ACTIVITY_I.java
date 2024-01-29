dd5c4ab813d6ff847f4dcb6d3693d7de8d86964a
Superangelo123 <157589468+Superangelo123@users.noreply.github.com>
2024-01-29 18:42:46 +0800

    Add files via upload

---
+++ Activities/Activity I/POGIO_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+public class POGIO_ACTIVITY_I {
+    public static void main (string[] args) {
+   
+         int x = 10;
+         int y = 20;
+         int z = 30;
+
+         int Product = x * y * z;
+         int Sum = x + y + z;
+         int Difference = x - y - z;
+         int Qoutient = x / y / z;
+
+          System.out println("Production:"+Product);
+          System.out println("Sum: "Sum);
+          System.out println("Difference; + Difference");
+          System.out println('Qoutient: "+ Qoutient');
+
+    }
+}    
+
+
