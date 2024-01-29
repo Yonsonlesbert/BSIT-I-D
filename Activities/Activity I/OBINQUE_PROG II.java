c23c5ca5a23252dc70749c2b81d7b20113177f6e
obinque <157590125+obinque@users.noreply.github.com>
2024-01-29 18:13:41 +0800

    Add files via upload

---
+++ Activities/Activity I/OBINQUE_PROG II.java
@@ -0,0 +1,22 @@
+public class OBINQUE_ACTIVITY I
+    public static void main(String[] args) {
+        int a = 10,b=20, c=30;
+        
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quo = a / c / b;
+
+
+        System.out.println("The sum is " + sum);
+        System.out.println("The difference is " + diff);
+        System.out.println("The product is " + prod );
+        System.out.println("The qoutient is " + quo );
+        
+    }
+}
+        
+
+        
+    
+}
