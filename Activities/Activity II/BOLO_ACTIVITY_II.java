05ccc067b9a484b7107ad50ea5ec0586cce8a9c9
jhonzkie201 <157588320+jhonzkie201@users.noreply.github.com>
2024-01-29 19:04:11 +0800

    Add files via upload

---
+++ Activities/Activity II/BOLO_ACTIVITY_II.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class BOLO_ACTIVITY_II {
+     public static void main(String []args){
+        Scanner sc = nextLine(System.in);
+
+        int = num1;
+        int = num2;
+
+        System.out.println("Enter the values");
+        num1 = sc.nextInt();
+        System.out.println("Enter the values");
+        num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+
+        System.out.println("Sum: "+ sum);
+        System.out.println("diff: "+ diff);
+        System.out.println("prod: "+ prod);
+        
+
+
+ 
+
+
+
+    
+
+        
+
+
+    }
+}
