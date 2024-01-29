a0396b6e70d1260f24c1d4eef52ff4b72990d463
gilcabalida <158039028+gilcabalida@users.noreply.github.com>
2024-01-29 18:34:46 +0800

    Add files via upload

---
+++ Activities/Activity I/CABALIDA_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class CABALIDA_ACTIVITY_I {
+    public static void main(String[] args) {
+        
+        int a= 10, b=20, c=30;
+
+        int addition = a+b+c;
+        int subtarction = a-c;
+        int multiplication = a*b;
+        int division = c/a;
+
+        System.out.println("Sum " + addition);
+        System.out.println("Diff " + subtarction);
+        System.out.println("Pro " + multiplication);
+        System.out.println("Quo " + division);
+
+        
+    }
+
+    
+}
