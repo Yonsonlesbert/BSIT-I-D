c617291e50e82a7f4299709528d7a41dd5ed17ff
Yonsonlesbert <157588466+Yonsonlesbert@users.noreply.github.com>
2024-01-29 18:12:51 +0800

    Add files via upload

---
+++ YONSON_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+#INCLUDE <iostream>
+public class YONSON_ACTIVITY_I {
+      public static void main(String[] args);
+      int a = 10, b = 15, c = 20;
+
+      int sum = a + b + c;
+      int diff = a - b;
+      int quo = b \ c;
+      int pro = a * b;
+
+      System.out.println("Sum: " + sum);
+      System.out.println("Difference: " + diff);
+      System.out.println("Quotient: " + quo);
+      System.out.println("Product: " + pro);
+}
+
+
