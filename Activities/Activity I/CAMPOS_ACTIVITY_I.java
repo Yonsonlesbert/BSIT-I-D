fc18ee78297971e565dc50f6184bec82260c4d3e
Kelrizz26 <158041133+Kelrizz26@users.noreply.github.com>
2024-01-29 18:38:21 +0800

    Add files via upload

---
+++ Activities/Activity I/CAMPOS_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class CAMPOS_ACTIVITY_I {
+    public static void main(String []args){
+
+    int a = 20, b = 30, c = 50;   
+     
+    int sum = a + b;
+    int prod = c * b;
+    int diff = b - a;
+    int quotient = c / a;
+    
+    System.out.println  (" Sum = " +sum);
+    System.out.println  ( " Prod = "+prod);
+    System.out.println  ( "Diff = " +diff);
+    System.out.println  ( "Quotient = " +quotient);
+
+    
+   }
+}
