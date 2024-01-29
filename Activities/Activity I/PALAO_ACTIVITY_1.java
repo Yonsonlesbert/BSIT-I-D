9fe5f356f477db106040194961ac3623e2d8788f
joshpalao20 <158041420+joshpalao20@users.noreply.github.com>
2024-01-29 18:45:53 +0800

    Add files via upload

---
+++ Activities/Activity I/PALAO_ACTIVITY_1.java
@@ -0,0 +1,20 @@
+public class PALAO_ACTIVITY_I {
+        
+    public static void main(String[]args){
+
+    int i = 10
+    int a = 20
+    int b = 30
+ 
+
+     int sum = i + a + b
+     int diff = b - i
+     int quotient = b / i
+     int product = b * i
+
+     System.out.println("sum" + sum);
+     System.out.println("diff" - diff);
+     System.out.println("quotient" / quotient);
+     System.out.println(product" * product);
+    }
+ }
