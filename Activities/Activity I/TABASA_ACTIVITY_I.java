6415c3904305f4fe6dbf865d35cd6408c4c624ad
johnalberttaro <158041430+johnalberttaro@users.noreply.github.com>
2024-01-29 18:35:37 +0800

    Add files via upload

---
+++ Activities/Activity I/TABASA_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class TABASA_ACTIVITY_I {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 18;
+        int c = 20;
+
+        int sum = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The difference is: " + diff);
+        System.out.println("The product is: " + prod);
+        System.out.println("The quotient is: " + quot);
+    }
+}
