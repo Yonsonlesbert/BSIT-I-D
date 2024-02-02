99d22829f0e21486ab4d4d18f3ebb543679d160b
warr90 <158041134+warr90@users.noreply.github.com>
2024-02-02 08:56:06 +0800

    Add files via upload

--- Activities/Activity I/public class DELFIN_ACTIVITY_I.java
+++ Activities/Activity I/public class DELFIN_ACTIVITY_I.java
@@ -1,19 +1,13 @@
 public class DELFIN_ACTIVITY_I.java;
 
-public static Variables {
+public static VariablesCalculator {
 public static void main (String []args) {
-       int a = 25;
-       int b = 50;
-       int c = 75;
+       int x = 25, int y = 50, int z = 75;
        
-       int sum = a + b;
-       int diff = c - b;
-       int prod = a * b * c;
-       int qout = a / b / c;
        
-       System.out.println("Sum: + sum");
-       System.out.println("Diff: - diff");
-       System.out.println("Prod: * prod");
-       System.out.println("Quot: / qout");
+       System.out.println("Sum of x+y+z " + sum);
+       System.out.println("Diff of z-y " - diff);
+       System.out.println("Prod of x*z*y " * prod);
+       System.out.println("Quot of z/y/x " / qout);
    } 
 }
