4b82860e492c6dfc2767a68b65cc7acad2b46cd0
warr90 <158041134+warr90@users.noreply.github.com>
2024-02-02 09:25:11 +0800

    Update and rename public class DELFIN_ACTIVITY_I.java to  DELFIN_ACTIVITY_I.java

---
+++ Activities/Activity I/ DELFIN_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class DELFIN_ACTIVITY_I.java;
+
+ public static void main(String[] args) {
+        int y = 25, x = 50, z = 75;
+       
+        int sum = y + x + z;
+        int diff = z - x - y;
+        int prod = y * z * x;
+        int quot = z / y;
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " +diff);
+        System.out.println("Product: " + prod);
+        System.out.println("Quotient: " + quot);
+    }
+}
+  
