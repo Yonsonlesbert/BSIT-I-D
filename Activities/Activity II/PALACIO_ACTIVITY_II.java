e44e77d3f2005bbff182cacb0339defc9bfcdede
Joelpalacio <157589826+Joelpalacio@users.noreply.github.com>
2024-02-02 08:33:42 +0800

    Add files via upload

---
+++ Activities/Activity II/PALACIO_ACTIVITY_II.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PALACIO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("First Number: ");
+        int fn = sc.nextInt();
+        System.out.print("Second Number: ");
+        int sn = sc.nextInt();
+
+        int Sum = fn + sn;
+        int Diff = fn - sn;
+        int Pro = fn * sn;
+        int Quo = fn / sn;
+
+        System.out.println("Sum: "+ Sum);
+        System.out.println("Diff: "+ Diff);
+        System.out.println("Pro: "+ Pro);
+        System.out.println("Quo: "+ Quo);
+    }
+    
+}
