7bff52bc94692e3b42686995499869b65fb7a667
patrick0907 <158041084+patrick0907@users.noreply.github.com>
2024-02-02 08:58:52 +0800

    Add files via upload

---
+++ Activities/Activity I/FAJARDO_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+public class FAJARDO_ACTIVITY_I {
+   public static void main(String []args){
+    int a = 10, b = 15, c = 20;
+     
+    int sum = a + b;
+    int prod = c * b;
+    int diff = b - a;
+    int quotient = c / a;
+    
+     System.out.println  (" Sum = " +sum);
+     System.out.println  ( " Prod = "+prod);
+     System.out.println  ( "Diff = " +diff);
+     System.out.println  ( "Quotient = " +quotient);
+    
+   }
+}
