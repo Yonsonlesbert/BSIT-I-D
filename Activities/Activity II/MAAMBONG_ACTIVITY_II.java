d1aff671063895862aef7cdb3a6cbae77dd357c7
Joshua00110 <156908549+Joshua00110@users.noreply.github.com>
2024-02-05 12:27:25 +0800

    Add files via upload

---
+++ Activities/Activity II/MAAMBONG_ACTIVITY_II.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class MAAMBONG_ACTIVITY_II {
+    public static void main (String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter first number : ");
+    int a = sc.nextInt();
+    System.out.print("Enter second number : ");
+    int b = sc.nextInt();
+
+    int sum = a+b;
+    int diff = a-b;
+    int prod = a*b;
+
+    System.out.println("Sum is : "+sum);
+    System.out.println("Difference is : "+diff);
+    System.out.println("Product is : "+prod);
+
+
+
+
+
+
+    }
+}
