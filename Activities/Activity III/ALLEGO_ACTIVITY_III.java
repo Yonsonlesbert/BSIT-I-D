782aa0e0b4ea1b331ecfd8cdf3fe6ea0b2562c85
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-02-05 18:19:04 +0800

    Add files via upload

---
+++ Activities/Activity III/ALLEGO_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ALLEGO_ACTIVITY_III {
+
+public static void main(String[]args){
+    Scanner sc =new Scanner(System.in);
+    System.out.print( " Input Temperature : ");
+   
+    int temp = sc.nextInt();
+    if(temp>=100){
+       System.out.println( "Boiling");
+     }else {
+        System.out.println( " Not Boiling ");
+     }
+    
+
+
+  }
+}
