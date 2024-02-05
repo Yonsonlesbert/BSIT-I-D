400e27524687f9dad29e9a096c32d4ae86e6a633
JOHNCHRISTOPHERPIEDAD <158041306+JOHNCHRISTOPHERPIEDAD@users.noreply.github.com>
2024-02-05 17:45:24 +0800

    Add files via upload

---
+++ Activities/Activity II/PIEDAD_ACTIVITY_II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PIEDAD_ACTIVITY_II {
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+		int number;
+		System.out.print(" Enter Firts number: ");
+		System.out.print(" Enter Second number: ");
+      	System.out.print(" Enter Third number: ");
+      	number = input.nextInt();
+      	System.out.println(" sum "+ sum);
+      	 	System.out.println(" diff "+);
+      	 	 	System.out.println(" product "+);
+      	 	 	 	System.out.println(" quotient "+);
+
+		}
+	}
