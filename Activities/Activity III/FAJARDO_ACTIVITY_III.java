a2ea73423e5b3ca8a113e8a6dea61d99a4b80ea3
patrick0907 <158041084+patrick0907@users.noreply.github.com>
2024-02-05 18:27:41 +0800

    Add files via upload

---
+++ Activities/Activity III/FAJARDO_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FAJARDO_ACTIVITY_III {
+    public static void main(String[]args){
+        int temp;
+      
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the temperature (in celcius): ");
+            temp = input.nextInt();
+            if (temp >= 100){
+                System.out.println("Boiling");
+            }else
+            System.out.println("Not boiling");
+
+
+        }
+    }
+
+        
