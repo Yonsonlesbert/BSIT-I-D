1576114a0c7922c285a18ac58726fd141da71443
Daveboi123 <157589344+Daveboi123@users.noreply.github.com>
2024-02-05 18:32:35 +0800

    Add files via upload

---
+++ Activities/Activity I/ARINGAY_ACTIVITY_1 {.java
@@ -0,0 +1,14 @@
+public class ARINGAY_ACTIVITY_1 {
+    public static void main(String[] args) {
+        int x = 15, y = 30, z = 10;
+
+        int sum = x + y + z;
+        int diff = z - y -x;
+        int prod = x * y * z;
+        int qout = z / y/ x;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Diff: "+diff);
+        System.out.println("Prod: "+prod);
+        System.out.println("Qout:"+qout);
+    }
