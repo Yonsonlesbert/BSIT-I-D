6a026d58860683532cb327a4c3ead74de486601d
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-02-05 18:33:31 +0800

    Add files via upload

---
+++ Activities/Activity III/CARREDO ACTIVITY 3.java
@@ -0,0 +1,14 @@
+import java.util.scanner
+public class CARREDO ACTIVITY 3{
+     public static void main(string[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Temperature : ");
+         Int temp = sc.nextInt();
+
+         if (temp >= 100 )
+           System.out.println("Boiling");
+     }else
+            System.out.println("Not Boiling");
+
+    }
+}
