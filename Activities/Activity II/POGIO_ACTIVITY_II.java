7a87b4af35781f76b1f3ddbaac61547a06f9827f
Superangelo123 <157589468+Superangelo123@users.noreply.github.com>
2024-02-05 17:53:17 +0800

    Add files via upload

---
+++ Activities/Activity II/POGIO_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class POGIO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first number:");
+        int fnum = sc.nextInt();
+        System.out.print("Enter your second number:");
+        int snum = sc.nextInt();
+
+        int sum = fnum + snum;
+        int diff = fnum - snum;
+        int pro = fnum * snum;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + pro);
+
+
+            
+    }
+    
