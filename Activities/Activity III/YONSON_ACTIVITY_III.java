02fedda09e04d0cf8fd458f7171d3f4e600c536a
Yonsonlesbert <157588466+Yonsonlesbert@users.noreply.github.com>
2024-02-05 18:24:32 +0800

    Add files via upload

---
+++ Activities/Activity III/YONSON_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+import java.util,Scanner;
+
+public class MAASIN_ACTIVITY_III{
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter Temperature : ");
+         Int temp = sc.nextInt();
+
+         if (temp >= 100){
+            System.out.println("Boiling");
+    }else
+            System.out.println("Not Boiling")  
+         }
+    }
