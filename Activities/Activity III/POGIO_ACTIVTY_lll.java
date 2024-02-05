1a09e1e3974dcdfd44c95eaabd20659844dd470e
Superangelo123 <157589468+Superangelo123@users.noreply.github.com>
2024-02-05 18:26:18 +0800

    Add files via upload

---
+++ Activities/Activity III/POGIO_ACTIVTY_lll.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class POGIO_ACTIVITY_III {
+    public static void main(String[]args){
+        int temp;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the temperature (in Celsius)");
+        temp = input.nextInt();
+            if(temp >= 100){
+                System.out.println("Boiling");
+            }else
+            System.out.println("Not boiling");
+        
+    }    
+}
