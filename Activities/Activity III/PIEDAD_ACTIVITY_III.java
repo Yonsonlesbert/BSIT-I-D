580c370d7a70b8b665f9829293969efa237005a8
brianpiedad225 <158042967+brianpiedad225@users.noreply.github.com>
2024-02-05 18:51:26 +0800

    Add files via upload

--- Activities/Activity III/PIEDAD_ACTIVITY_III.java
+++ Activities/Activity III/PIEDAD_ACTIVITY_III.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
-public class PIEDAD_ACTIVITY_III {
-    public static void main(String[]args){
-        int temp;
-        
-            Scanner input  = new Scanner(System.in);
-            System.out.print("Enter the temperature (in celcius): ");
-            temp = input.nextInt();
-            if (temp >= 100){
-                System.out.println("Boiling");
-            }else
-            System.out.println("Not Boiling");
-
-      
-    }
-}
-        
+public class PIEDAD_ACTIVITY_III{
+	public static void main(String[]args){
+		Scanner input = new Scanner (System.in);
+		int temperature;
+		System.out.print("Enter your Temperature: ");
+		int tem = input.nextInt();
+		if (tem<=105){
+			System.out.println("Boiling ");
+		}
+		else{
+		System.out.println("Not Boiling");
+		}
+	  }
+	}
