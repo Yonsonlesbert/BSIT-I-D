5f426162c864e12c5e2ba5718aa9642e9e17e2eb
carl160 <156910039+carl160@users.noreply.github.com>
2024-02-05 18:18:58 +0800

    Add files via upload

---
+++ Activities/Activity III/PAUSTOR_ACTIVITY_lll.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+public class PAUSTOR_ACTIVITY_III{ 
+    public static void main(string[]args) {
+         Scanner temp=new Scanner(System.in);
+         System.out.prinln(s:"Temperature=");
+         Double temps = temp.nextDouble();
+         if(temp>=100){
+            System.out.println(s:"Bolling");
+         }else
+         System.out.println(s:"Not bolling");
+         
+
+         
+    }
+}
