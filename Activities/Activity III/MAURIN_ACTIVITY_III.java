789ca870b32d9d28dd3139bd52e24da815aba4fe
shekinamariemaurin <156909161+shekinamariemaurin@users.noreply.github.com>
2024-02-05 18:20:43 +0800

    Add files via upload

---
+++ Activities/Activity III/MAURIN_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MAURIN_ACTIVITY_III{
+
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+    int temp;
+
+    System.out.print("Enter Celcius: " );
+    temp = sc.nextInt();
+
+    if(temp>=100){
+        System.out.println("Boiling ");
+
+    }else{
+        System.out.println("Not Boiling ");
+        }
+    }
+}
