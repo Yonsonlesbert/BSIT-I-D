afae02df1ce2c95f239ccedb614fdb291c3c2902
johnkenneth1 <157591295+johnkenneth1@users.noreply.github.com>
2024-02-05 18:37:54 +0800

    Add files via upload

---
+++ Activities/Activity I/UDIAMAN_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class UDIAMAN_ACTIVITY_I {
+
+ public static void main(String[] args) {
+        int y = 25, x = 50, z = 75;
+       
+        int sum = y + x + z;
+        int diff = z - x - y;
+        int prod = y * z * x;
+        int quot = z / y;
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " +diff);
+        System.out.println("Product: " + prod);
+        System.out.println("Quotient: " + quot);
+    }
+}
+  
