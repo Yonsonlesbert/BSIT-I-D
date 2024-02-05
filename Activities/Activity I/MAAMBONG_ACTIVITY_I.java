cee7a5ef455c3a3ed10f891c3b493a31ce4d8b7c
Joshua00110 <156908549+Joshua00110@users.noreply.github.com>
2024-02-05 12:17:32 +0800

    Add files via upload

---
+++ Activities/Activity I/MAAMBONG_ACTIVITY_I.java
@@ -0,0 +1,19 @@
+public class MAAMBONG_ACTIVITY_I {
+    public static void main (String []args ) {
+    int a = 50, b = 30, c = 20;
+    
+    int sum = a+b+c;
+    int diff = a-b-c;
+    int prod = a*b*c;
+    int quo = a/b/c;
+
+    System.out.println("Sum is : "+sum);
+    System.out.println("Difference is : "+diff);
+    System.out.println("Product is : "+prod);
+    System.out.println("Quotient is : "+quo);
+
+
+
+
+    }
+}
