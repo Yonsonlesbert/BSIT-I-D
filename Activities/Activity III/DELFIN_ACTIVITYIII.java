a531f0d99191a59cb1071947ee8210493db94b64
warr90 <158041134+warr90@users.noreply.github.com>
2024-02-05 18:19:44 +0800

    Add files via upload

---
+++ Activities/Activity III/DELFIN_ACTIVITYIII.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public static void main(String []args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Your Celsius: ");
+    int temp = sc.nextInt();
+
+    if (temp>=110){
+        System.out.println("Boiling");
+    } else {
+        System.out.println("Not Boiling");
+    }
+}
+}
