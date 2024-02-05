a5e793eeaea367d9892f07790d3c6c4d786d420c
James Ivan Jimenez <156911118+jamesezrampage12@users.noreply.github.com>
2024-02-05 18:20:21 +0800

    Add files via upload

---
+++ Activities/Activity III/JIMENEZ_ACTIVITY_III.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class JIMENEZ_ACTIVITY_III{
+    public static void main(String []args){
+    Scanner input = new Scanner(System.in);
+    int temp;
+
+    System.out.print("Enter Celsius: ");
+    temp = input.nextInt();
+    if (temp >=100){
+
+    System.out.println("Boiling");
+    
+    }else
+    System.out.println("Not Boiling");
+
+
+
+
+
+    }
+
+}
