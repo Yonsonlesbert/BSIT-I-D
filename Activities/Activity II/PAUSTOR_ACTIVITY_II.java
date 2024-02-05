e68509e9ce9ca4d6f1713ec2ccb23701d736a498
carl160 <156910039+carl160@users.noreply.github.com>
2024-02-05 17:46:32 +0800

    Add files via upload

---
+++ Activities/Activity II/PAUSTOR_ACTIVITY_II.java
@@ -0,0 +1,27 @@
+import.java.util.Scanner;
+  public class PAUSTOR_ACTIVITY_II{ 
+    public static void main(String[] args) {
+        Scanner myNum=new Scanner(System.in);
+        int sum,sub,mult;
+       
+
+        System.out.println(x:"Enter your first Number:");
+        int num1=myNum.nextInt();
+
+        System.out.println(x:"Enter your second number:");
+        int num2=myNum nextlnt();
+          int num2-PAUSTOR_ACTIVITY_II.main(String[])
+           sum=num1+num2
+           sub=num1-num2
+           mult=num1*num2;
+
+           System.out.println("Sum:"+sum );
+           System.out.println("Diff:"+sub );
+           System.out.println("Product" +mult );
+    }
+
+
+  }
+    
+        
+    
