24c687d845b876b4f0e7e398828919a456008129
obinque <157590125+obinque@users.noreply.github.com>
2024-02-05 18:21:09 +0800

    Add files via upload

---
+++ Activities/Activity III/OBINQUE_ACTIVITY_III.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class OBINQUE_ACTIVITY_III{
+
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+        int temp;
+
+        System.out.print("Enter Celcius: ");
+        temp = sc.nextInt();
+
+        if(temp>=100){
+            System.out.println("Boiling");
+    
+        }else{
+
+            System.out.println("Not Boiling");
+        }
+
+    }
+}
