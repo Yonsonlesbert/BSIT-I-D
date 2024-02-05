199193a97b8878d837dff7cdcb0d90506b23ad82
Estudillostephanie <157589357+Estudillostephanie@users.noreply.github.com>
2024-02-05 18:28:07 +0800

    Update ESTUDILLO_ACTIVITY_II.java

--- Activities/Activity II/ESTUDILLO_ACTIVITY_II.java
+++ Activities/Activity II/ESTUDILLO_ACTIVITY_II.java
@@ -1,16 +1,21 @@
-import java.util.Scanner;
-
-public class ESTUDILLO_ACTIVITY_II{
-
-    public static void main(String []args){
-        Scanner sc = new Scanner (System.in);
+public class ESTUDILLO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-        int num1;
-        int num2;
+        System.out.print(s:"Enter your first number: ");
+        int num1 = sc.nextInt();
+        System.out.print(s:"Enter your second number: ");
+        int num2 = sc.nextInt();
 
-        System.out.print("Enter VAlues: ");
-        System.out.print("Enter")
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
 
-       
+        System.out.println("Sum =" + sum);
+        System.out.println("Difference =" + diff);
+        System.out.println("Product =" + prod);
+
+
     }
+
 }
