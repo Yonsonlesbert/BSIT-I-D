d80323bfed2ffda7959b776b0120ad293fd89131
brianpiedad225 <158042967+brianpiedad225@users.noreply.github.com>
2024-02-07 18:43:02 +0800

    Add files via upload

---
+++ PIEDAD_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+public class Piedad {
+	public static void main (String []args){
+		int a = 40, b = 5;
+		int sum = a+b;
+		int difference = a-b;
+		int product = a*b;
+		int quotient = a/b;
+		int modulo = a%b;
+		System.out.println(" Sum "+ (a+b));
+        System.out.println(" Difference "+(a-b));
+        System.out.println(" Product "+(a*b));
+        System.out.println(" Quotient "+(a/b));
+    }
+ }
