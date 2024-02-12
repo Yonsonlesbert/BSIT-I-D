cee3e55e0141662743ad3fc9e335117392951387
Yonsonlesbert <157588466+Yonsonlesbert@users.noreply.github.com>
2024-02-12 18:12:56 +0800

    Add files via upload

---
+++ Activities/Activity V/YONSON_ACTIVITY_V.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class YONSON_ACTIVITY_V.java{
+
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter your age: ");
+
+        int age = sc.nextInt();
+        if(age<=11){
+            System.out.printl("Child");
+        }
+            
+        else if (age>12 && age<17){
+            System.out.println("Teen");
+        }
+            
+        else if(age>18 && age<64){
+            System.out.println("Adult");
+        }
+            
+
+        
+            
+    }
+}
