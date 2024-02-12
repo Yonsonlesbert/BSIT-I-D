51075f30001f7d5d0fea1ff07473f692478ab483
LuckyCarredo <159015794+LuckyCarredo@users.noreply.github.com>
2024-02-12 18:14:21 +0800

    Add files via upload

---
+++ Activities/Activity V/CARREDO_ACTIVITY_V.java
@@ -0,0 +1,21 @@
+import java.util.Scanner
+ public class CARREDO_ACTIVITY_IIIII {
+    public static void main(String []args) {
+     Scanner ages = new Scanner.in);
+     Int age;
+     System.out.print(s:"Input your age");
+     age=ages.nextInt();
+
+     if (age < 12) {
+     System.out.println("Child");
+    }
+    else {
+     if (age < 18) {
+     System.out.println("Teen");   
+    }
+    else {
+    System.out.println("Adult");
+     }
+   }  
+  }
+}
