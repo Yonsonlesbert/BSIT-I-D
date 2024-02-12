46471f3cda5dd7bed829ddec922651b7b757e651
Leonises <157588921+calunsagleonises@users.noreply.github.com>
2024-02-12 18:13:40 +0800

    Add files via upload

---
+++ Activities/Activity V/CALUNSAG_ACTIVITY_V.java
@@ -0,0 +1,19 @@
+public class CALUNSAG_ACTIVITY_V{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in)
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt;
+        
+        if(age<11){
+            System.out.print("Child");
+
+        }
+        if else (age<=12&&age<=17){
+            System.out.print("Teen");
+
+        if else (age>=18&&age<=64){
+            System.out.print("Adult");
+        }
+    
+    }
+}
