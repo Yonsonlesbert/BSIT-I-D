ebf9541a0c5f3185c5f80af18edc9e9cb05daba7
Balongcas <157291749+AljhoBalongcas@users.noreply.github.com>
2024-02-12 18:13:12 +0800

    Add files via upload

---
+++ Activities/Activity V/BALONGCAS_ACTIVITY_V.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class BALONGCAS_ACTIVITY_V{
+    public static void main(String[] args) {
+        Scanner ages = new Scanner(System.in);
+        int age;
+        System.out.print("Input your age: ");
+        age = ages.nextInt();
+
+        if (age>=0 && age>=11){
+            System.out.print("Child");
+        }
+        else if (age>=12 && age>=17)
+        System.out.print("Teen");
+        else if (age>=18 && age>=64)
+        System.out.print("Adult");
+       
+    }
+}
