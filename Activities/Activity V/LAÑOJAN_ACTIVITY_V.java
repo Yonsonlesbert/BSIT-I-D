41cb657d8000021cb624a67b710d99ff134dc50c
Iamjoseph21 <156908222+Iamjoseph21@users.noreply.github.com>
2024-02-12 18:12:39 +0800

    Add files via upload

---
+++ Activities/Activity V/LAÑOJAN_ACTIVITY_V.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LAÑOJAN_ACTIVITY_V {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Age:");
+         int ages = sc.nextInt();
+        if  (ages >=0 && ages <=11){
+        System.out.println("Child");
+        } else if ( ages >=12 && ages <=17){
+        System.out.println("Teen");
+         }
+          else {
+        System.out.println("Adult");
+    }
+        } 
+            
+        }
+
+
+
+    
+
