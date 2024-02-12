c5f30dbf3153b6cce918cdf08f91b8c3dd22a133
allegofaith <156910544+allegofaith@users.noreply.github.com>
2024-02-12 18:12:46 +0800

    Add files via upload

---
+++ Activities/Activity V/ALLEGO_ACTIVITY_V.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ALLEGO_ACTIVITY_V {
+   public static void main (String []args){
+   Scanner sc = new Scanner(System.in);
+
+   System.out.print( "Enter your age : ");
+   int age = sc.nextInt();
+
+   if( age<=11 && age>=0){
+    System.out.println(" Child ");
+    } else if ( age<=17 && age>=12){
+     System.out.println(" Teen");   
+    }else{
+      System.out.println(" Adult ");
+    }
+  }
+
+}
+    
+
